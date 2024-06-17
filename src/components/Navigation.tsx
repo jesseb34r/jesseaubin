8405711389e4c9a1b23708195b9601908129a57b
jesseb34r <61254589+jesseb34r@users.noreply.github.com>
2024-06-16 21:19:17 -0700

    feat: add initial navigation menu

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,14 +1,28 @@
-import { NavigationMenu } from "@kobalte/core/navigation-menu";
-
 export function Navigation() {
   return (
-    <NavigationMenu class="flex">
-      <NavigationMenu.Menu>
-        <NavigationMenu.Trigger class="flex-grow">Jesse Aubin</NavigationMenu.Trigger>
-        <NavigationMenu.Trigger>About</NavigationMenu.Trigger>
-        <NavigationMenu.Trigger>Uses</NavigationMenu.Trigger>
-        <NavigationMenu.Trigger>Notes</NavigationMenu.Trigger>
-      </NavigationMenu.Menu>
-    </NavigationMenu>
+    <nav>
+      <ul class="flex max-w-2xl mx-auto my-margin gap-gutter">
+        <li class="flex-grow">
+          <a href="/" class="text-xl font-semibold hover:text-plum-dim transition-colors">
+            Jesse Aubin
+          </a>
+        </li>
+        <li>
+          <a href="/about" class="hover:underline hover:font-semibold">
+            About
+          </a>
+        </li>
+        <li>
+          <a href="/uses" class="hover:underline hover:font-semibold">
+            Uses
+          </a>
+        </li>
+        <li>
+          <a href="/notes" class="hover:underline hover:font-semibold">
+            Notes
+          </a>
+        </li>
+      </ul>
+    </nav>
   );
 }
