9c8a511e6ee43d8cee56aa757a0ecd4108fedded
jesseb34r <61254589+jesseb34r@users.noreply.github.com>
2024-06-16 21:04:19 -0700

    feat: add custom sizing to tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,27 @@
-/** @type {import('tailwindcss').Config} */
-
 import tailwindcssRadixColors from "tailwindcss-radix-colors";
 
+const size = {
+  xs: "4.25rem",
+  sm: "9.75rem",
+  md: "15.25rem",
+  lg: "20.75rem", // 4 columns (mobile breakpoint). Never set min-size to larger than this
+  xl: "26.25rem",
+  "2xl": "31.75rem", // 6 columns, half of laptop breakpoint
+  "3xl": "37.25rem",
+  "4xl": "42.75rem",
+  "5xl": "48.75rem",
+  "6xl": "53.75rem",
+  "7xl": "59.25rem",
+  "8xl": "64.75rem",
+  mobile: "20.75rem",
+  tablet: "42.75rem",
+  laptop: "64.75rem",
+  ultrawide: "86.75rem",
+  gutter: "1.25rem",
+  margin: "0.625rem",
+};
+
+/** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
@@ -29,6 +49,7 @@ export default {
         "monospace",
       ],
     },
+    width: size,
     extend: {},
   },
   plugins: [tailwindcssRadixColors],
