e18342b953331ec36f309b2c35ecef1967b108dd
jesseb34r <jesseb34r@jesseaubin.me>
2025-08-26 22:02:40 -0700

    migrate to astro 5 and add biome

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,22 +3,28 @@ export function Navigation() {
     <nav class="mx-margin">
       <ul class="flex m-auto gap-gutter mb-gutter">
         <li class="flex-grow">
-          <a href="/" class="text-xl font-semibold hover:text-plum-dim transition-colors text-nowrap">
+          <a
+            class="text-xl font-semibold hover:text-plum-dim transition-colors text-nowrap"
+            href="/"
+          >
             Jesse<span class="max-tablet:hidden"> Aubin</span>
           </a>
         </li>
         <li>
-          <a href="/about" class="hover:underline hover:font-semibold">
+          <a class="hover:underline hover:font-semibold" href="/about">
             About
           </a>
         </li>
         <li>
-          <a href="/uses" class="hover:underline hover:font-semibold">
+          <a class="hover:underline hover:font-semibold" href="/uses">
             Uses
           </a>
         </li>
         <li>
-          <a href="/notes/about-these-notes" class="hover:underline hover:font-semibold">
+          <a
+            class="hover:underline hover:font-semibold"
+            href="/notes/about-these-notes"
+          >
             Notes
           </a>
         </li>
